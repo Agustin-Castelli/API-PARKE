f8fa0d08b8a40f9bffcd95f210f9809a23c768b8
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-11-10 05:12:34 -0300

    API terminada

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9c2b710ec06219e4db11bab10cb67ca47f9da5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc720b48fe986dc34c580bedf65d6d14b1a5607")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
